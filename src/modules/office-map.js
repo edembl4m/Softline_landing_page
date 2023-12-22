31fa7253b23e1bc5b9664adae3fd0d8985a7b360
edembl4m <mametov.edem@bk.ru>
2023-12-22 23:34:05 +0300

    office map submenu

--- src/modules/office-map.js
+++ src/modules/office-map.js
@@ -1,8 +1,29 @@
 const officeBtn = document.querySelector('.office-map__btn');
-const officeBtnArrow = document.querySelector('.office-map__btn-arrow')
+const officeBtnArrow = document.querySelector('.office-map__btn-arrow');
+const officeMapSubmenu = document.querySelector('.office-map__submenu');
+// const officeMapSubmenuTitle = document.querySelectorAll('.office-map__submenu-item-title');
+const officeMapListItem = document.querySelectorAll('.office-map__list-item');
+
+officeBtn.addEventListener('click', () => openSubmenu());
 
 
-officeBtn.addEventListener('click', () => {
+function openSubmenu() {
     officeBtnArrow.classList.toggle('arrow-active');
-    console.log(1);
+    officeMapSubmenu.classList.toggle('submenu-active');
+}
+
+function closeTabs() {
+    officeMapListItem.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+function toggleSelect(tab) {
+    closeTabs();
+    tab.classList.add('active');
+}
+
+
+officeMapListItem.forEach(item => {
+    item.addEventListener('click', () => toggleSelect(item))
 });
