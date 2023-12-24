ed576e62137b6ad791b529f9533d18264f4875d6
edembl4m <mametov.edem@bk.ru>
2023-12-24 03:41:21 +0300

    office map logic

--- src/modules/office-map.js
+++ src/modules/office-map.js
@@ -1,26 +1,60 @@
 const officeBtn = document.querySelector('.office-map__btn');
 const officeBtnArrow = document.querySelector('.office-map__btn-arrow');
 const officeMapSubmenu = document.querySelector('.office-map__submenu');
-// const officeMapSubmenuTitle = document.querySelectorAll('.office-map__submenu-item-title');
 const officeMapListItem = document.querySelectorAll('.office-map__list-item');
 
-officeBtn.addEventListener('click', () => openSubmenu());
 
+const allCity = document.querySelectorAll('.office-map__map-mark')
+const moscow = document.querySelectorAll('.moscow');
+const center = document.querySelectorAll('.center');
+const northwest = document.querySelectorAll('.northwest');
+const south = document.querySelectorAll('.south');
+const volga = document.querySelectorAll('.volga');
+const ural = document.querySelectorAll('.ural');
+const siberia = document.querySelectorAll('.siberia');
+const farEast = document.querySelectorAll('.far-east');
+
+const zonesObj = {
+    'allCity': allCity,
+    'moscow': moscow,
+    'center': center,
+    'northwest': northwest,
+    'south': south,
+    'volga': volga,
+    'ural': ural,
+    'siberia': siberia,
+    'farEast': farEast,
+}
+
+
+//кнопка Офисы Softline
+officeBtn.addEventListener('click', () => openSubmenu());
 
 function openSubmenu() {
     officeBtnArrow.classList.toggle('arrow-active');
     officeMapSubmenu.classList.toggle('submenu-active');
 }
 
-function closeTabs() {
-    officeMapListItem.forEach(item => {
-        item.classList.remove('active');
-    })
+
+
+function closeTabs(index) {
+    for(let i = 0; i < officeMapListItem.length; i++) {
+        officeMapListItem[i].classList.remove('active');
+        allCity.forEach(item => {
+            item.classList.remove('mark-show')
+        })
+
+    }
 }
 
 function toggleSelect(tab) {
-    closeTabs();
+    closeTabs(tab.id);
     tab.classList.add('active');
+    console.log(tab.id);
+    zonesObj[tab.id].forEach(item => {
+        item.classList.add('mark-show');
+    });
+
 }
 
 
